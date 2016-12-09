74d4f686370fc2ca63d1a5838a0eb85906c89c0c
Ryan Lutgen <ryan.lutgen@InfiniteCampus.com>
2016-12-09 14:32:56 -0600

    Put back in logger now that dependency seems to have been resolved.

--- src/main/java/org/elasticsearch/plugin/elasticfence/logger/EFLogger.java
+++ src/main/java/org/elasticsearch/plugin/elasticfence/logger/EFLogger.java
@@ -1,7 +1,7 @@
 package org.elasticsearch.plugin.elasticfence.logger;
 
-//import org.apache.logging.log4j.Logger;
-//import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.logging.ESLoggerFactory;
 
 /**
  * simple logger class for elasticfence. 
@@ -10,28 +10,28 @@ package org.elasticsearch.plugin.elasticfence.logger;
  */
 public class EFLogger {
     //ESLogger (which this was) no longer exists, the ESLoggerFactory getLogger now returns a log4j logger instance
-//	private static Logger esLogger;
+	private static Logger esLogger;
 	static {
-//		esLogger = ESLoggerFactory.getLogger("plugin.elasticfence");
+		esLogger = ESLoggerFactory.getLogger("plugin.elasticfence");
 	}
 
 	public static void debug(String msg) {
-//		esLogger.debug(msg);
+		esLogger.debug(msg);
 	}
 
 	public static void info(String msg) {
-//		esLogger.info(msg);
+		esLogger.info(msg);
 	}
 
 	public static void warn(String msg) {
-//		esLogger.warn(msg);
+		esLogger.warn(msg);
 	}
 
 	public static void error(String msg) {
-//		esLogger.error(msg);
+		esLogger.error(msg);
 	}
 	
 	public static void error(String msg, Exception cause) {
-//		esLogger.error(msg, cause);
+		esLogger.error(msg, cause);
 	}
 }
